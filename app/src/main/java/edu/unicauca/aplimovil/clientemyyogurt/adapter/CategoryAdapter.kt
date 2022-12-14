4892d84a77fc1f1b367b330c0acec5ddfacbf131
enriqueman <manzano.velasco217@gmail.com>
2022-12-14 17:06:33 -0500

    Revert "Merge branch 'main' of https://github.com/enriqueman/ClienteMyYogurt"
    This reverts commit d3af17cab50ced6ad405dcb03c501360e3fb7aa8, reversing
    changes made to 4c3c16c8f27e57af748457b2105afa3a900081e3.

--- app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/adapter/CategoryAdapter.kt
+++ app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/adapter/CategoryAdapter.kt
@@ -1,36 +1,28 @@
 package edu.unicauca.aplimovil.clientemyyogurt.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import edu.unicauca.aplimovil.clientemyyogurt.R
-import edu.unicauca.aplimovil.clientemyyogurt.activity.CategoryActivity
 import edu.unicauca.aplimovil.clientemyyogurt.databinding.LayoutCategoryItemBinding
 import edu.unicauca.aplimovil.clientemyyogurt.model.CategoryModel
 
 
-class CategoryAdapter(var context: Context,var list: ArrayList<CategoryModel>): RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class CategoryAdapter(var contex: Context,var list: ArrayList<CategoryModel>): RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
     inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view){
         var binding = LayoutCategoryItemBinding.bind(view)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-        return CategoryViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_category_item,parent,false))
+        return CategoryViewHolder(LayoutInflater.from(contex).inflate(R.layout.layout_category_item,parent,false))
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        holder.binding.textView.text = list[position].cate
-        Glide.with(context).load(list[position].img).into(holder.binding.imageView)
-        holder.itemView.setOnClickListener(){
-            val intent = Intent(context, CategoryActivity:: class.java)
-            intent.putExtra("cate", list[position].cate)
-            context.startActivity(intent)
-        }
-
+    holder.binding.textView.text = list[position].cate
+    Glide.with(contex).load(list[position].img).into(holder.binding.imageView)
     }
 
     override fun getItemCount(): Int {
