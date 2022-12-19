a1aa68fb829d4718136f9f86e0a6b67f76f5c7c1
LadyK-t <67769472+LadyK-t@users.noreply.github.com>
2022-12-19 16:29:55 -0500

    Carrito de compras con boton
    Inicio de colocar el precio del carro de compras

--- app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/adapter/CartAdapter.kt
+++ app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/adapter/CartAdapter.kt
@@ -1,13 +1,19 @@
 package edu.unicauca.aplimovil.clientemyyogurt.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import edu.unicauca.aplimovil.clientemyyogurt.activity.YogurtDetailsActivity
 
 import edu.unicauca.aplimovil.clientemyyogurt.databinding.LayoutCartItemBinding
+import edu.unicauca.aplimovil.clientemyyogurt.roomdb.AppDatabase
 import edu.unicauca.aplimovil.clientemyyogurt.roomdb.YogurtModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CartAdapter(val context: Context, val list: List<YogurtModel>) :
 RecyclerView.Adapter<CartAdapter.CartViewHolder>(){
@@ -24,7 +30,30 @@ RecyclerView.Adapter<CartAdapter.CartViewHolder>(){
        Glide.with(context).load(list[position].yogurtImages).into(holder.binding.imageView4)
         holder.binding.textView7.text = list[position].yogurtName
         holder.binding.textView12.text = list[position].yogurtPrecio
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, YogurtDetailsActivity::class.java)
+            intent.putExtra("id",list[position].yogurtId)
+            context.startActivity(intent)
+        }
+        val dao = AppDatabase.getInstance(context).yogurtDao()
+        holder.binding.imageView5.setOnClickListener{
+            GlobalScope.launch (Dispatchers.IO){
+                dao.deleteYogurt(
+                    YogurtModel(
+                        list[position].yogurtId,
+                        list[position].yogurtName,
+                        list[position].yogurtImages,
+                        list[position].yogurtPrecio
+                    )
+                )
+            }
+          //  dao.deleteYogurt(YogurtModel(list[position].yogurtId,list[position].yogurtName,list[position].yogurtImages,list[position].yogurtPrecio))
+        }
+
+
     }
+
 
     override fun getItemCount(): Int {
 
