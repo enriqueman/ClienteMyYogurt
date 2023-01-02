5bf7da329ce5ac70518c6f34a67ae531948b25a8
enriqueman <manzano.velasco217@gmail.com>
2023-01-02 01:44:26 -0500

    Configuracion de carrito

--- app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/fragment/CartFragment.kt
+++ app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/fragment/CartFragment.kt
@@ -1,5 +1,6 @@
 package edu.unicauca.aplimovil.clientemyyogurt.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import edu.unicauca.aplimovil.clientemyyogurt.R
+import edu.unicauca.aplimovil.clientemyyogurt.activity.AddressActivity
+import edu.unicauca.aplimovil.clientemyyogurt.activity.YogurtDetailsActivity
 import edu.unicauca.aplimovil.clientemyyogurt.adapter.CartAdapter
 import edu.unicauca.aplimovil.clientemyyogurt.databinding.FragmentCartBinding
 import edu.unicauca.aplimovil.clientemyyogurt.roomdb.AppDatabase
+import edu.unicauca.aplimovil.clientemyyogurt.roomdb.YogurtModel
 
 class CartFragment : Fragment() {
     private lateinit var binding: FragmentCartBinding
@@ -28,9 +32,26 @@ class CartFragment : Fragment() {
         val dao = AppDatabase.getInstance(requireContext()).yogurtDao()
         dao.getAllYogurt().observe(requireActivity()){
             binding.cartRecycler.adapter= CartAdapter(requireContext(),it)
+            totalcost(it)
         }
 
         return binding.root
+    }
+
+    private fun totalcost(data: List<YogurtModel>?) {
+        var total= 0
+        for(item in data!!){
+            total+=item.yogurtPrecio!!.toInt()
+        }
+        binding.textView13.text="Numero de productos: ${data.size}"
+        binding.textView14.text="Costo total: $total"
+
+        binding.checkout.setOnClickListener {
+            val intent = Intent(context, AddressActivity::class.java)
+            intent.putExtra("totalcost", total)
+            startActivity(intent)
+        }
+
     }
 
 
