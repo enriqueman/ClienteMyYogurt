6ce35beacebca7f40f4d3c4e643be6ef52e61b17
enriqueman <manzano.velasco217@gmail.com>
2023-01-03 00:24:01 -0500

    login inicial

---
+++ app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/activity/OTPActivity.kt
@@ -0,0 +1,12 @@
+package edu.unicauca.aplimovil.clientemyyogurt.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.unicauca.aplimovil.clientemyyogurt.R
+
+class OTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpactivity)
+    }
+}
