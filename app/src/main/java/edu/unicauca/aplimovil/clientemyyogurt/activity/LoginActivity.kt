2cc2cf96cd28dbcacadb00bb007adffe9ba9120f
enriqueman <manzano.velasco217@gmail.com>
2023-01-04 23:17:38 -0500

    Update login

--- app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/activity/LoginActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/activity/LoginActivity.kt
@@ -1,12 +1,34 @@
 package edu.unicauca.aplimovil.clientemyyogurt.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import edu.unicauca.aplimovil.clientemyyogurt.R
+import edu.unicauca.aplimovil.clientemyyogurt.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding =ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button5.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish()
+        }
+        
+        binding.button4.setOnClickListener { 
+            if (binding.userNumber.text!!.isEmpty())
+                Toast.makeText(this,"Por favor ingrese un numero",Toast.LENGTH_SHORT).show()
+            else
+                sendOtp(binding.userNumber.text.toString())
+
+        }
+    }
+
+    private fun sendOtp(toString: String) {
+
     }
 }
--- app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/activity/RegisterActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/clientemyyogurt/activity/RegisterActivity.kt
@@ -1,12 +1,67 @@
 package edu.unicauca.aplimovil.clientemyyogurt.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import edu.unicauca.aplimovil.clientemyyogurt.R
+import edu.unicauca.aplimovil.clientemyyogurt.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding =ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button4.setOnClickListener {
+
+            openLogin()
+
+        }
+
+        binding.button5.setOnClickListener {
+            validateUser()
+        }
+    }
+
+    private fun validateUser() {
+        if (binding.Username.text!!.isEmpty() || binding.userNumber.text!!.isEmpty())
+            Toast.makeText(this, "Por favor, ingrese todos los campos", Toast.LENGTH_SHORT).show()
+        else
+            storeData()
+    }
+
+    private fun storeData() {
+        var builder =AlertDialog.Builder(this)
+            .setTitle("Cargando... ")
+            .setMessage("Por favor espere")
+            .setCancelable(false)
+            .create()
+        builder.show()
+        val data = hashMapOf<String,Any>()
+        data["name"]=binding.Username.text.toString()
+        data["number"]=binding.userNumber.text.toString()
+
+        Firebase.firestore.collection("users").document(binding.Username.text.toString())
+            .set(data).addOnSuccessListener {
+                Toast.makeText(this,"Usuario registrado",Toast.LENGTH_SHORT).show()
+                openLogin()
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"Algo salio mal",Toast.LENGTH_SHORT).show()
+
+            }
+
+    }
+
+    private fun openLogin() {
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
+
     }
 }
